c637e24c07093f3322c15bab8232834c12ac4750
skaznik <skaznik@wszib.edu.pl>
2019-11-17 08:37:50 +0100

    dodatkowy komentarz

--- src/Git.java
+++ src/Git.java
@@ -1,5 +1,7 @@
 public class Git {
     public static void main(String[] args) {
         System.out.println("Witaj w Git!!!!");
+
+        // to jest komentarz ... program nie robi nic, tylko wyświetla "Witaj w Git"
     }
 }
